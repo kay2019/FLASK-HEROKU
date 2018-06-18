fdd10c8ed415d2b6cc3eec97279124ab84e42b46
kay2019 <36146231+kay2019@users.noreply.github.com>
2018-06-18 11:52:12 -0400

    Update app9.py

--- app9.py
+++ app9.py
@@ -28,7 +28,6 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 class ReusableForm(Form):
     name = TextField('Thick data:', validators=[validators.required()])
  
- 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     form = ReusableForm(request.form)
@@ -37,40 +36,25 @@ def hello():
     if request.method == 'POST':
         name=request.form['name']
         print(request.form.getlist('box'))
-#        listOfOptions=request.form.getlist('box')
-#        n=len(listOfOptions)
+        listOfOptions=request.form.getlist('box')
+        n=len(listOfOptions)
         start=datetime.datetime(2016,1,1)
         end=datetime.datetime(2018,1,1)
-        stk = quandl.get("WIKI/" + "GOOGL", start_date=start, end_date=end)
-#        
-#
-# 
-#        color=["black","blue","red","green"]
-#        plt.figure(figsize=(8,8))
-#        for i in range(n):
-#            plt.plot(pd.DataFrame(data=stk[listOfOptions[i]]),"b-",color[i])
-#
-#            
-#        plt.legend(loc="upper right")
-#        plt.xlabel("Dates")
-#        plt.ylabel(name)        
+        stk = quandl.get("WIKI/" + name, start_date=start, end_date=end)
 
+        color=["black","blue","red","green"]
+     
         p1 = figure(x_axis_type="datetime", title="Stock Closing Prices")
 
-
-        p1.line(stk.index,stk["Close"])
-        
+        for i in range(n):
+            p1.line(stk.index,stk[listOfOptions[i]],color=color[i])
+            
         show(p1) 
 
         script, div = components(p1)
-   
-
-        
+ 
         if form.validate():
-
-           
-            
-            
+      
             flash('Hey ' + name)
         else:
             flash('All the form fields are required. ')
